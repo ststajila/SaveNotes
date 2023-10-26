2bda7c6819e6bcae4eb7ade2e3724e7285424501
Stanislav <stazilastas@gmail.com>
2023-10-26 09:51:43 -0500

    Save to the Phone Notes

--- SaveNotes/ViewController.swift
+++ SaveNotes/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreInput: UITextField!
+    //Step 1
+    // creating a reference to the phone creating a variable you can save to
+    let defaults = UserDefaults.standard
+    var name = ""
 
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+   
     override func viewDidLoad() {
+        //Step 3
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //getting the value from the phone
+        name = defaults.string(forKey: "theName") ?? ""
+        labelOutlet.text = name
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        
+       var text = textFieldOutlet.text!
+        //Step 2
+        //saving to the phone
+        defaults.set(text, forKey: "theName")
+        let myScore = scoreInput.text 
+        
+    }
+    
 }
 
